e0657b1ddb9044b525c2ad56f1c9d1ac6804d2f6
JPCassimiro <jp.cassimironf@gmail.com>
2025-06-14 15:39:09 -0300

    a

--- database_descriptions/schema.sql
+++ database_descriptions/schema.sql
@@ -1,20 +1,33 @@
+create table user (
+	user_id SERIAL PRIMARY KEY,
+	user_email VARCHAR(255) UNIQUE NOT NULL,
+	user_password VARCHAR(255) NOT NULL,
+	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+ )
+
 create table image (
 	image_id SERIAL PRIMARY KEY,
 	image_name VARCHAR(255) NOT NULL,
 	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, 
-	image_file bytea NOT NULL
+	image_file bytea NOT NULL,
+	user_id INTEGER NOT NULL,
+	CONSTANT fk_user FOREIGN KEY (user_id) REFERENCES user(user_id)
+		ON DELETE CASCADE
 );
 
 CREATE table album (
 	album_id SERIAL PRIMARY KEY,
 	album_name VARCHAR(255) NOT NULL,
 	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-	album_preview INTEGER 
+	album_preview INTEGER
+	user_id INTEGER NOT NULL,
+	CONSTANT fk_user FOREIGN KEY (user_id) REFERENCES user(user_id)
+		ON DELETE CASCADE
 );
 
 CREATE TABLE album_image_junction (
-	album_id SERIAL NOT NULL,
-	image_id SERIAL NOT NULL,
+	album_id INTEGER NOT NULL,
+	image_id INTEGER NOT NULL,
 	PRIMARY KEY(album_id, image_id),
 	CONSTRAINT fk_image FOREIGN KEY (image_id) REFERENCES image(image_id)
 		ON DELETE CASCADE,
@@ -22,4 +35,3 @@ CREATE TABLE album_image_junction (
 		ON DELETE CASCADE
 );
 
-	
